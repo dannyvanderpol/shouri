6dc6b192e5d5cb76225ecb58b7d409502657e9fb
Danny <danny.v.der.pol@gmail.com>
2024-12-31 20:30:56 +0100

    add application logger

--- application/modules/general/controllerApplication.php
+++ application/modules/general/controllerApplication.php
@@ -4,6 +4,18 @@
 
 class ControllerApplication extends F\ControllerBase
 {
+    public function executeAction($action, $level, $parameters)
+    {
+        define("APP_LOG", new F\ModelLogger("application"));
+        APP_LOG->writeMessage("------------------------------ Application start ------------------------------");
+        APP_LOG->writeMessage("Action    : '{$action}'");
+        APP_LOG->writeMessage("Level     : {$level}");
+        APP_LOG->writeMessage("Parameters:");
+        APP_LOG->writeDataArray($parameters);
+
+        return $this->$action($parameters);
+    }
+
     protected function showWrongUri()
     {
         return $this->showPage("WrongUri");
