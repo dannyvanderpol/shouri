09d6aa547f50f2952713ff1eac3305f1fd03955c
Danny <danny.v.der.pol@gmail.com>
2025-01-01 13:56:19 +0100

    add model for reading and writing the configuration file

---
+++ application/modules/general/modelConfiguration.php
@@ -0,0 +1,44 @@
+<?php
+
+/* Model for handling the configuration */
+
+
+class ModelConfiguration
+{
+    const configFile = ABS_PATH . CONFIG_FOLDER . "config.php";
+
+    const defaultConfig = [
+        "server"        => "localhost",
+        "database"      => "",
+        "username"      => "",
+        "password"      => "",
+        "ssl_domain"    => ""
+    ];
+
+
+    public static function writeConfigurationFile($configData)
+    {
+        // Merge with default data, missing data will have default values and can be changed
+        $configData = array_merge(self::defaultConfig, $configData);
+        $output = "<?php\n\n";
+        $output .= "/* Configuration file for your server settings */\n\n";
+        $output .= "\$configurationData = ";
+        $output .= var_export($configData, true);
+        $output .= ";\n";
+        file_put_contents(self::configFile, $output);
+    }
+
+    public static function readConfigurationFile()
+    {
+        $configData = self::defaultConfig;
+        if (file_exists(self::configFile))
+        {
+            ob_start();
+            include self::configFile;
+            ob_get_clean();
+            // Merge defaults with configuration data to make sure all config data exists
+            array_merge($configData, $configurationData);
+        }
+        return $configData;
+    }
+}
