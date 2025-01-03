8d41fb3689cb9970b7904b8579ce48064fb32b7c
Danny <danny.v.der.pol@gmail.com>
2025-01-03 16:42:56 +0100

    add path for modules folder

--- application/initialize.php
+++ application/initialize.php
@@ -5,6 +5,7 @@ require_once("templateValues.php");
 
 define("PAGE_TITLE",            "Shouri");
 define("CONFIG_FOLDER",         "config/");
+define("MODULES_FOLDER",        "application/modules");
 define("STYLES_FOLDER",         "application/styles/");
 
 define("DATABASE_VERSION",      "0.1");
@@ -12,4 +13,4 @@ define("DEFAULT_TIME_ZONE",     "Europe/Amsterdam");
 define("DEFAULT_LANDING_PAGE",  "dashboard");
 define("DEFAULT_COLOR",         "#0066aa");
 
-define("SEARCH_PATHS",          ["application/modules"]);
+define("SEARCH_PATHS",          [MODULES_FOLDER]);
